d65ebbe623a4678f0084909d4fa5173315b38063
Basem Khalaf <bkhalaf94@gmail.com>
2022-01-23 23:49:36 +0200

    Add files via upload

---
+++ pipeline.py
@@ -0,0 +1,17 @@
+import sys   
+#to be able to print on cli
+
+import pandas as pd 
+
+#to print this on CLI
+print(sys.argv)
+
+day = sys.argv[1]
+#cli argument number 1
+
+#some fancy staff with pandas
+
+print(f'Job finished succesfully for day = f{day}')
+#finial message after entring the day 
+
+
